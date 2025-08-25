9d6e200e209354c930674fb1b42af0253bd27416
mimifuwa <mimifuwa.cc@gmail.com>
2025-08-26 02:13:22 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold font-mono">
+          {window.location.hostname}
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
